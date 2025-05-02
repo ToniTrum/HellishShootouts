c7d8db82aaeb393ccb14b8873ae235e0ef12539e
ToniTrum <kulahszjan@gmail.com>
2025-05-02 18:21:54 +1000

    add direction change in moving for enemies

--- Assets/Scrpts/Enemy/EnemyAnimation.cs
+++ Assets/Scrpts/Enemy/EnemyAnimation.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EnemyAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
-
+    [SerializeField] private SpriteRenderer _spriteRenderer;
     private EnemyMovement _enemyMovement;
 
     private readonly int _isWalkingHash = Animator.StringToHash("IsWalking");
@@ -11,6 +11,7 @@ public class EnemyAnimation : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _enemyMovement = GetComponent<EnemyMovement>();
     }
 
@@ -18,5 +19,11 @@ public class EnemyAnimation : MonoBehaviour
     {
         bool isWalking = _enemyMovement.GetVelocity().magnitude > 0.1f;
         _animator.SetBool(_isWalkingHash, isWalking);
+
+        if (isWalking)
+        {
+            Vector2 velocity = _enemyMovement.GetVelocity();
+            _spriteRenderer.flipX = velocity.x < 0;
+        }
     }
 }
